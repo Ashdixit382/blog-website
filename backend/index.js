b6016df159e8ba7af2a77b0eca4ccc458b6adafa
Ashdixit382 <ashishdixit601@gmail.com>
2024-07-16 15:01:31 +0530

    Backend Portion 90% complete

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const dotenv = require('dotenv')
+const cookieParser=require('cookie-parser')
+const authRoute=require('./routes/auth.js')
+const userRoute=require('./routes/users.js')
+const postRoute = require('./routes/posts.js')
+const commentRoute=require('./routes/comments')
+
 
 const connectdb = async() => {
      try{
-        await mongoose.connect("mongodb+srv://johndixit575:ashish1@cluster0.lgcx6jw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("database connect successfully")
      }
      catch(err){
@@ -12,7 +19,17 @@ const connectdb = async() => {
      }
 }
 
-app.listen(6000,()=>{
+dotenv.config()
+
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
+app.use("/api/posts",postRoute)
+app.use("/api/comments",commentRoute)
+
+app.listen(process.env.PORT,()=>{
     connectdb();
-    console.log("app is running on port 5000");
+    console.log("app is running on port "+process.env.PORT);
 })
